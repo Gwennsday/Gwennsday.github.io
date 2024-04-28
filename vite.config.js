c0c509529634cd19eec97a0027fc2efe474c4e06
von Schappler <37213588+vonschappler@users.noreply.github.com>
2024-04-27 21:25:22 -0300

    Small fix to update the main branch

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import eslint from "vite-plugin-eslint";
 export default defineConfig({
   // plugins: [react(), eslint()],
   plugins: [react(), eslint()],
-  base: '/'
+  base: "/gwennsday.github.io/",
 });
